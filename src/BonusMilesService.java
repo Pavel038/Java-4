026c0fd85d1138e7a731f35eed2e8692eaf02b2d
Pavel038 <98365181+Pavel038@users.noreply.github.com>
2022-12-22 21:31:40 +0700

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,11 +1,9 @@
 public class BonusMilesService {
     public int calculate(int cost) {
         int result;
-        {
-            int y = 20; // количесво рублей для 1 мили
-            int miles = cost / y;
-            result = miles;
-        }
+        int y = 20; // количесво рублей для 1 мили
+        int miles = cost / y;
+        result = miles;
         return result;
     }
 }
